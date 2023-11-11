7732d905b994f2e799b517eb80b173cb47a41d8f
restuadkh <restuadimas11@yahoo.com>
2023-11-11 12:56:57 +0700

    Upload

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Halaman Utama</h1>
+      <p>Selamat datang di halaman utama!</p>
+      <i class="fa fa-home" aria-hidden="true"></i>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const Login = ({ handleLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validasi login, contoh sederhana
+    if (username === 'user' && password === 'password') {
+    //   handleLogin(username);
+      setUsername('');
+      setPassword('');
+      setError('');
+    } else {
+      setError('Username atau password salah');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
